68e66e70bf3c7cef6b27af8ab757572ef0b29905
DenisKaminsky <deniskaminsky123@mail.ru>
2019-09-18 00:24:31 +0300

    fix report lab 1

--- 1/V6_Task2.sql
+++ 1/V6_Task2.sql
@@ -1,19 +1,18 @@
 USE AdventureWorks2012;
 GO
 
-/*Select all departments which start with 'F' and end with 'e'*/
+/*Вывести на экран список отделов, названия которых начинаются на букву ‘F’ и заканчиваются на букву ‘е’.*/
 SELECT d.DepartmentID, d.Name 
 FROM HumanResources.Department d
 WHERE d.Name LIKE 'F%e';
 
-/*Select average count of Vacation hours and average of Sick leave hours*/
+/*Вывести на экран среднее количество часов отпуска и среднее количество больничных часов у сотрудников. 
+Назовите столбцы с результатами ‘AvgVacationHours’ и ‘AvgSickLeaveHours’ для отпусков и больничных соответственно.*/
 SELECT AVG(e.VacationHours) 'AvgVacationHours', AVG(e.SickLeaveHours) 'AvgSickLeaveHours'
 FROM HumanResources.Employee e;
 
-/*Select employees older than 65 and their years worked*/
+/*Вывести на экран сотрудников, которым больше 65-ти лет на настоящий момент. Вывести также количество лет, 
+прошедших с момента трудоустройства, в столбце с именем ‘YearsWorked’.*/
 SELECT e.BusinessEntityID, e.JobTitle, e.Gender ,DATEDIFF(YEAR, e.HireDate, CURRENT_TIMESTAMP) 'YearsWorked' 
 FROM HumanResources.Employee e
 WHERE DATEDIFF(YEAR, e.BirthDate , CURRENT_TIMESTAMP) > 65;
-
-
-
