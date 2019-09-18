3050d9a9c24bac70278c1796fddabda4782e63af
DenisKaminsky <deniskaminsky123@mail.ru>
2019-09-19 00:12:21 +0300

    fix lab 2

--- 2/V6_Task1.sql
+++ 2/V6_Task1.sql
@@ -1,14 +1,15 @@
 USE AdventureWorks2012;
 GO
 
-/*Select the earliest employee start date in each department*/
+/*Вывести на экран самую раннюю дату начала работы сотрудника в каждом отделе. Дату вывести для каждого отдела.*/
 SELECT d.Name, MIN(e.HireDate) 'StartDate'
 FROM HumanResources.Employee e
 INNER JOIN HumanResources.EmployeeDepartmentHistory ed ON ed.BusinessEntityID = e.BusinessEntityID
 INNER JOIN HumanResources.Department d ON d.DepartmentID = ed.DepartmentID
 GROUP BY d.Name;
 
-/*Select all employees with job title 'Stocker' and their ShiftIndex*/
+/*Вывести на экран название смены сотрудников, работающих на позиции ‘Stocker’. 
+Замените названия смен цифрами (Day — 1; Evening — 2; Night — 3).*/
 SELECT e.BusinessEntityID, e.JobTitle, s.ShiftID 'ShiftName'
 FROM HumanResources.Employee e
 INNER JOIN HumanResources.EmployeeDepartmentHistory ed ON ed.BusinessEntityID = e.BusinessEntityID
@@ -16,7 +17,8 @@ INNER JOIN HumanResources.Shift s ON s.ShiftID = ed.ShiftID
 WHERE e.JobTitle = 'Stocker';
 
 
-/* Select all employees with current department and replace 'and' on '&' in JobTitle */
+/*Вывести на экран информацию обо всех сотрудниках, с указанием отдела, в котором они работают в настоящий момент.
+В названии позиции каждого сотрудника заменить слово ‘and’ знаком & (амперсанд). */
 SELECT e.BusinessEntityID, REPLACE(e.JobTitle, 'and', '&') 'Job Title', d.Name 'DepName'
 FROM HumanResources.Employee e
 INNER JOIN HumanResources.EmployeeDepartmentHistory ed ON ed.BusinessEntityID = e.BusinessEntityID
